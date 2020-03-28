21dfff18090d8b0557971c7a1a15d010e4a945b5
Jay McDoniel <jmcdo29@gmail.com>
2020-03-28 11:52:48 -0700

    feat(socket.io): implements parser for socket.io
    The SocketioInterceptorParser has two hardcoded values, one for method and one for protocol. It also
    only has the concept of pass (200) and fail (500), though there could be a case for checking if the
    error is an `HttpException` and if so getting the status from there like is done in the HttpParsers.
    
    fix #22

---
+++ integration/socket.io/testScript.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const socket = require('socket.io-client')('http://[::1]:3000');
+
+function cb(...args) {
+  args.forEach((arg) => console.log(arg));
+}
+
+function socketCall(message) {
+  socket.emit(message, '', cb);
+}
+
+socketCall('message');
+socketCall('throw');
+socketCall('skip');
+
+setTimeout(() => socket.disconnect(), 500);
