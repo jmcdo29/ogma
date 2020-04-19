9de8c96ab1b71ca56e73613e2379a6df98187203
Jay McDoniel <jmcdo29@gmail.com>
2020-04-19 00:01:23 -0700

    feat(ws): implements parser for websocket
    The WsParser is now written in its entirity. There are a few hardcoded values, though those could be
    overriden by dev if they choose by extending the parser itself. Tests are written (P.S. writing
    tests to satisfy Socket.io **and** Websocket was almost impossible, but I got it done). Only thing
    to do from here is to finish off the microservice parsers :smile:
    
    fix #24

--- integration/src/ws/ws.filter.ts
+++ integration/src/ws/ws.filter.ts
@@ -1,10 +1,15 @@
 import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
 import { BaseWsExceptionFilter } from '@nestjs/websockets';
+import WebSocket = require('ws');
 
 @Catch()
 export class ExceptionFilter extends BaseWsExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const client = host.switchToWs().getClient();
-    client.emit('exception', exception.message);
+    if (client instanceof WebSocket) {
+      return client.send('exception');
+    } else {
+      return client.emit('exception', exception.message);
+    }
   }
 }
