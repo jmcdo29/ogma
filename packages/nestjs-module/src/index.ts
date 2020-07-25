c380ae95bd4c85aacb85c66806a8a7ab1765a922
Jay McDoniel <jmcdo29@gmail.com>
2020-07-25 12:02:03 -0700

    feat(module): adds export for `createProviderToken`
    The export for the `createProviderToken` can be made use of in tests or manual usages to get the
    Ogma provider token (like in factories) so that developers no longer need to know what the
    dependency injection token is.

--- packages/nestjs-module/src/index.ts
+++ packages/nestjs-module/src/index.ts
@@ -3,4 +3,5 @@ export * from './interceptor/ogma.interceptor';
 export * from './interceptor/providers/abstract-interceptor.service';
 export * from './interfaces/ogma-options.interface';
 export * from './ogma.module';
+export { createProviderToken } from './ogma.provider';
 export * from './ogma.service';
