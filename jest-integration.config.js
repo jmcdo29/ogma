4fc3804153822dc4351df00ab43e2a14ac9eb7fb
Jay McDoniel <jmcdo29@gmail.com>
2020-11-08 17:02:06 -0800

    ci(all): adds coverage report for CI

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: './integration',
+  moduleFileExtensions: ['ts', 'js'],
+  rootDir: '.',
   testRegex: '.spec.ts$',
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testEnvironment: 'node',
+  collectCoverageFrom: ['packages/**/src/**/*.ts', '!**/index.ts'],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '@ogma/(.*)': '<rootDir>/packages/$1/src',
+  },
 };
