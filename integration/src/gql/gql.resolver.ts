41fee9db624dbdd7223da0f964dcb009902e9964
Jay McDoniel <jmcdo29@gmail.com>
2021-01-01 01:04:38 -0800

    fix(gql): interceptors now work with subscriptions

--- integration/src/gql/gql.resolver.ts
+++ integration/src/gql/gql.resolver.ts
@@ -1,12 +1,15 @@
 import { BadRequestException } from '@nestjs/common';
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { OgmaSkip } from '@ogma/nestjs-module';
+// import { PubSub } from 'graphql-subscriptions';
 import { AppService } from '../app.service';
 import { SimpleObject } from './simple-object.model';
 
 @Resolver(() => SimpleObject)
 export class GqlResolver {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService, // @Inject('PUB_SUB') private readonly pubSub: PubSub,
+  ) {}
 
   @Query(() => SimpleObject)
   getQuery(): SimpleObject {
@@ -26,6 +29,14 @@ export class GqlResolver {
   @OgmaSkip()
   @Query(() => SimpleObject)
   getSkip(): SimpleObject {
-    return this.appService.getHello();
+    // eslint-disable-next-line sonarjs/prefer-immediate-return
+    const obj = this.appService.getHello();
+    // this.pubSub.publish('subscribed', { subscribed: obj });
+    return obj;
   }
+
+  /* @Subscription(() => SimpleObject)
+  subscribed() {
+    return this.pubSub.asyncIterator('subscribed');
+  } */
 }
--- packages/platform-graphql/src/graphql-interceptor.service.ts
+++ packages/platform-graphql/src/graphql-interceptor.service.ts
@@ -19,4 +19,13 @@ export class GraphQLParser extends ExpressParser {
   getResponse(context: ExecutionContext): Response {
     return this.getContext(context).getContext().res;
   }
+
+  setRequestId(context: ExecutionContext, requestId: string): void {
+    const ctx = this.getContext(context).getContext();
+    if (ctx.req) {
+      ctx.req.requestId = requestId;
+    } else {
+      ctx.requestId = requestId;
+    }
+  }
 }
