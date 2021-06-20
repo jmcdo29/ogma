62e7269aef74430e072e89ef00e7e7710c671916
Jay McDoniel <jmcdo29@gmail.com>
2021-06-20 16:32:31 -0700

    chore: fixup integration test and package versions

--- integration/src/ws/ws.filter.ts
+++ integration/src/ws/ws.filter.ts
@@ -1,12 +1,11 @@
 import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
 import { BaseWsExceptionFilter } from '@nestjs/websockets';
-import WebSocket = require('ws');
 
 @Catch()
 export class ExceptionFilter extends BaseWsExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const client = host.switchToWs().getClient();
-    if (client instanceof WebSocket) {
+    if (client._isServer) {
       return client.send('exception');
     } else {
       return client.emit('exception', exception.message);
--- integration/test/utils/matcher.ts
+++ integration/test/utils/matcher.ts
@@ -2,7 +2,61 @@
 
 import { style } from '@ogma/styler';
 import { isIP } from 'net';
-import { LogObject } from '@ogma/nestjs-module/lib/interceptor/interfaces/log.interface';
+
+interface LogObject {
+  /**
+   * The IP or IP addresses of the machine making a call to the server
+   */
+  callerAddress: string[] | string;
+  /**
+   * REST: an HTTP Verb (GET, POST, PATCH, etc)
+   *
+   * GraphQL: Query, Mutation, or Subscription
+   *
+   * Microservice: Request or Reply
+   *
+   * Websockets: unknown at moment
+   */
+  method: string;
+  /**
+   * REST: endpoint
+   *
+   * GraphQL: Query or Mutation name
+   *
+   * Microservice: Message Topic
+   *
+   * WebSockets: Subscription Event name
+   */
+  callPoint: string;
+  /**
+   * REST: HTTP/majorVersion.minorVersion e.g. `HTTP/1.1`
+   *
+   * GraphQL: HTTP/majorVersion.minorVersion e.g. `HTTP/1.1`
+   *
+   * Microservice: microservice type e.g. amqp
+   *
+   * WebSockets: ws
+   */
+  protocol: string;
+  /**
+   * REST: HTTP Status code, will extract from Nest Exception if possible
+   *
+   * GraphQL: HTTP Status code, will extract from Nest Exception if possible
+   *
+   * Microservice: 200 for success, 500 for error
+   *
+   * Websockets: 200 for success, 500 for error
+   */
+  status: string;
+  /**
+   * The time it took the request, in milliseconds, from entering the interceptor to leaving the interceptor
+   */
+  responseTime: number;
+  /**
+   * Number of bytes in the response data, determined by Buffer encoding the response body
+   */
+  contentLength: number;
+}
 
 const timeRegex = /\d+ms/;
 const sizeRegex = /\d+/;
