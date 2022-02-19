bf02b3b52781008da4263b638c7acf10b1e5df7a
Jay McDoniel <jmcdo29@gmail.com>
2022-02-18 18:26:59 -0800

    test(module): change skip decorator tests to uvu
    Signed-off-by: Jay McDoniel <jmcdo29@gmail.com>

--- packages/nestjs-module/test/skip.decorator.spec.ts
+++ packages/nestjs-module/test/skip.decorator.spec.ts
@@ -1,20 +1,23 @@
+import { suite } from 'uvu';
+import { not, ok } from 'uvu/assert';
 import { OgmaSkip } from '../src';
 import { OGMA_INTERCEPTOR_SKIP } from '../src/ogma.constants';
 
-describe('OgmaSkip', () => {
-  it('should work on an entire class', () => {
-    @OgmaSkip()
-    class TestClass {}
-    expect(Reflect.getMetadata(OGMA_INTERCEPTOR_SKIP, TestClass)).toBe(true);
-  });
-  it('should work on a class method', () => {
-    class TestClass {
-      @OgmaSkip()
-      shouldSkip(): void {
-        console.log('do nothing');
-      }
-    }
-    const testClass = new TestClass();
-    expect(Reflect.getMetadata(OGMA_INTERCEPTOR_SKIP, testClass.shouldSkip)).toBe(true);
-  });
+const OgmaSkipSuite = suite('OgmaSkip');
+OgmaSkipSuite('It should work on an entire class', () => {
+  @OgmaSkip()
+  class TestClass {}
+  ok(Reflect.getMetadata(OGMA_INTERCEPTOR_SKIP, TestClass));
 });
+OgmaSkipSuite('It should work on a single method', () => {
+  class TestClass {
+    @OgmaSkip()
+    shouldSkip(): void {
+      console.log('do nothing');
+    }
+  }
+  const testClass = new TestClass();
+  ok(Reflect.getMetadata(OGMA_INTERCEPTOR_SKIP, testClass.shouldSkip));
+  not.ok(Reflect.getMetadata(OGMA_INTERCEPTOR_SKIP, TestClass));
+});
+OgmaSkipSuite.run();
