63d9374721cbe7a495a06dab62f3b2e97d021c44
Jay McDoniel <jmcdo29@gmail.com>
2022-02-18 21:25:15 -0800

    test(module): replace ogma interceptor tests with uvu

--- packages/nestjs-module/test/ogma.interceptor.spec.ts
+++ packages/nestjs-module/test/ogma.interceptor.spec.ts
@@ -1,14 +1,13 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
+import { spy, Stub, stubMethod } from 'hanbi';
 import { of, throwError } from 'rxjs';
+import { suite } from 'uvu';
+import { equal, is, ok } from 'uvu/assert';
 import { OgmaInterceptor, OgmaService, OgmaSkip } from '../src';
 import { DelegatorService } from '../src/interceptor/providers';
 import { OGMA_INTERCEPTOR_OPTIONS } from '../src/ogma.constants';
-
-const shouldSkipFor = (type: 'http' | 'ws' | 'gql' | 'rpc'): string =>
-  `should skip for no ${type} parser`;
 
 const nullifyOption = (type: 'http' | 'ws' | 'gql' | 'rpc', interceptor: OgmaInterceptor): void => {
   (interceptor as any).options[type] = false;
@@ -16,278 +15,297 @@ const nullifyOption = (type: 'http' | 'ws' | 'gql' | 'rpc', interceptor: OgmaInt
 const noop = () => {
   /* no-op */
 };
+const createCtxMock = (partial: Partial<ExecutionContext>): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: () =>
+    ({
+      name: 'something',
+    } as any),
+  getHandler: () =>
+    ({
+      name: 'something',
+    } as any),
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
+});
 
-const logMock = jest.fn();
-
-const httpContext = {
+const httpContext: Partial<ExecutionContext> = {
   getType: () => 'http',
   getArgs: () => [1, 2, 3],
   setRequestId: noop,
-};
+} as any;
 
-describe('OgmaInterceptor', () => {
-  let interceptor: OgmaInterceptor;
-  let delegator: DelegatorService;
-  let ogmaService: OgmaService;
-  let reflector: Reflector;
-
-  beforeEach(async () => {
-    const mod = await Test.createTestingModule({
-      providers: [
-        OgmaInterceptor,
-        {
-          provide: DelegatorService,
-          useValue: {
-            getContextSuccessString: () => ({
-              log: 'success',
-            }),
-            getContextErrorString: () => ({
-              log: 'error',
-            }),
-            setRequestId: noop,
-          },
+const OgmaInterceptorSuite = suite<{
+  interceptor: OgmaInterceptor;
+  delegator: DelegatorService;
+  logMock: Stub<OgmaService['info']>;
+  reflectorGetSpy: Stub<Reflector['get']>;
+}>('Ogma Interceptor Suite', {
+  interceptor: undefined,
+  delegator: undefined,
+  logMock: spy(),
+  reflectorGetSpy: spy(),
+});
+OgmaInterceptorSuite.before(async (context) => {
+  const mod = await Test.createTestingModule({
+    providers: [
+      OgmaInterceptor,
+      {
+        provide: DelegatorService,
+        useValue: {
+          getContextSuccessString: () => ({
+            log: 'success',
+          }),
+          getContextErrorString: () => ({
+            log: 'error',
+          }),
+          setRequestId: noop,
         },
-        {
-          provide: Reflector,
-          useValue: {
-            get: jest.fn(() => false),
-          },
+      },
+      {
+        provide: Reflector,
+        useValue: {
+          get: context.reflectorGetSpy.handler,
         },
-        {
-          provide: OgmaService,
-          useValue: {
-            info: logMock,
-            ogma: {
-              options: {
-                color: false,
-                json: false,
-              },
+      },
+      {
+        provide: OgmaService,
+        useValue: {
+          info: context.logMock.handler,
+          ogma: {
+            options: {
+              color: false,
+              json: false,
             },
           },
         },
-        {
-          provide: OGMA_INTERCEPTOR_OPTIONS,
-          useValue: {
+      },
+      {
+        provide: OGMA_INTERCEPTOR_OPTIONS,
+        useValue: {
+          http: true,
+        },
+      },
+    ],
+  }).compile();
+  context.delegator = mod.get(DelegatorService);
+  context.interceptor = mod.get(OgmaInterceptor);
+});
+OgmaInterceptorSuite.before.each(({ reflectorGetSpy }) => {
+  Date.now = () => 0;
+  reflectorGetSpy.returns(false);
+});
+OgmaInterceptorSuite.after.each(({ reflectorGetSpy, logMock }) => {
+  reflectorGetSpy.reset();
+  logMock.reset();
+});
+OgmaInterceptorSuite('It should be defined', ({ interceptor }) => {
+  ok(interceptor);
+});
+const callHandler = { handle: () => of('something') };
+OgmaInterceptorSuite(
+  'should log data in intercept',
+  async ({ interceptor, delegator, logMock }) => {
+    return new Promise<void>((resolve, reject) => {
+      const delegateSpy = stubMethod(delegator, 'getContextSuccessString');
+      delegateSpy.returns({ log: 'success' });
+      const ctxMock = createCtxMock(httpContext);
+      interceptor.intercept(ctxMock, callHandler).subscribe({
+        error: reject,
+        complete: () => {
+          is(delegateSpy.callCount, 1);
+          equal(delegateSpy.firstCall.args, [
+            'something',
+            ctxMock,
+            0,
+            {
+              color: false,
+              json: false,
+              http: true,
+            },
+          ]);
+          is(logMock.callCount, 1);
+          resolve();
+        },
+      });
+    });
+  },
+);
+OgmaInterceptorSuite(
+  'Should not log the data',
+  async ({ reflectorGetSpy, interceptor, logMock, delegator }) => {
+    return new Promise<void>((resolve, reject) => {
+      reflectorGetSpy.returns(true);
+      const delegateSpy = stubMethod(delegator, 'getContextSuccessString');
+      const ctxMock = createCtxMock(httpContext);
+      interceptor.intercept(ctxMock, callHandler).subscribe({
+        error: reject,
+        complete: () => {
+          is(delegateSpy.callCount, 0);
+          is(logMock.callCount, 0);
+          resolve();
+        },
+      });
+    });
+  },
+);
+OgmaInterceptorSuite(
+  'should log the data and meta',
+  async ({ delegator, interceptor, logMock }) => {
+    return new Promise<void>((resolve, reject) => {
+      const delegateSpy = stubMethod(delegator, 'getContextSuccessString');
+      delegateSpy.returns({
+        log: 'success',
+        meta: { key: 'value' },
+      });
+      const ctxMock = createCtxMock(httpContext);
+      interceptor.intercept(ctxMock, callHandler).subscribe({
+        error: reject,
+        complete: () => {
+          equal(delegateSpy.firstCall.args, [
+            'something',
+            ctxMock,
+            0,
+            {
+              color: false,
+              json: false,
+              http: true,
+            },
+          ]);
+          is(logMock.callCount, 2);
+          resolve();
+        },
+      });
+    });
+  },
+);
+const errorCallHandler = { handle: () => throwError(() => new Error('Big oof')) };
+OgmaInterceptorSuite('should log the error', async ({ interceptor, logMock, delegator }) => {
+  return new Promise<void>((resolve, reject) => {
+    const ctxMock = createCtxMock(httpContext);
+    const delegateSpy = stubMethod(delegator, 'getContextErrorString');
+    delegateSpy.returns({
+      log: 'error',
+    });
+    interceptor.intercept(ctxMock, errorCallHandler).subscribe({
+      next: reject,
+      error: () => {
+        is(delegateSpy.callCount, 1);
+        equal(delegateSpy.firstCall.args, [
+          new Error('Big oof'),
+          ctxMock,
+          0,
+          {
+            json: false,
+            color: false,
             http: true,
           },
-        },
-      ],
-    }).compile();
-    delegator = mod.get(DelegatorService);
-    interceptor = mod.get(OgmaInterceptor);
-    ogmaService = mod.get(OgmaService);
-    reflector = mod.get(Reflector);
-  });
-  afterEach(() => {
-    logMock.mockReset();
-  });
-  it('should be defined', () => {
-    expect(interceptor).toBeDefined();
-  });
-  describe('intercept', () => {
-    Date.now = () => 0;
-    describe('log data', () => {
-      let delegateSpy: jest.SpyInstance;
-      const callHandler = { handle: () => of('something') };
-      beforeEach(() => {
-        delegateSpy = jest.spyOn(delegator, 'getContextSuccessString');
-      });
-      afterEach(() => {
-        delegateSpy.mockClear();
-      });
-      it('should log data', (done) => {
-        const ctxMock = createMock<ExecutionContext>(httpContext as Partial<ExecutionContext>);
-        interceptor.intercept(ctxMock, callHandler).subscribe({
-          next: () => {
-            return;
-          },
-          error: () => {
-            throw new Error('Logging Error in data');
-          },
-          complete: () => {
-            expect(delegateSpy).toBeCalledWith('something', ctxMock, 0, {
-              color: false,
-              json: false,
-              http: true,
-            });
-            expect(logMock).toBeCalledTimes(1);
-            done();
-          },
-        });
-      });
-      it('should not log data', (done) => {
-        jest.spyOn(reflector, 'get').mockReturnValueOnce(true);
-
-        interceptor.intercept(createMock<ExecutionContext>(), callHandler).subscribe({
-          next: () => {
-            return;
-          },
-          error: () => {
-            throw new Error('Logging Error in data');
-          },
-          complete: () => {
-            expect(delegateSpy).toBeCalledTimes(0);
-            expect(logMock).toBeCalledTimes(0);
-            done();
-          },
-        });
-      });
-      it('should log the data and the meta', (done) => {
-        delegateSpy = jest.spyOn(delegator, 'getContextSuccessString').mockReturnValueOnce({
-          log: 'success',
-          meta: { key: 'value' },
-        });
-        const ctxMock = createMock<ExecutionContext>(httpContext as Partial<ExecutionContext>);
-        interceptor.intercept(ctxMock, callHandler).subscribe({
-          next: () => {
-            return;
-          },
-          error: () => {
-            throw new Error('Logging Error in data');
-          },
-          complete: () => {
-            expect(delegateSpy).toBeCalledWith('something', ctxMock, 0, {
-              color: false,
-              json: false,
-              http: true,
-            });
-            expect(logMock).toBeCalledTimes(2);
-            done();
-          },
-        });
-      });
-    });
-    describe('log error', () => {
-      let delegateSpy: jest.SpyInstance;
-      const callHandler = { handle: () => throwError(() => new Error('Big oof')) };
-      beforeEach(() => {
-        delegateSpy = jest.spyOn(delegator, 'getContextErrorString');
-      });
-      afterEach(() => {
-        delegateSpy.mockClear();
-      });
-      it('should log error', (done) => {
-        const ctxMock = createMock<ExecutionContext>(httpContext as Partial<ExecutionContext>);
-        interceptor.intercept(ctxMock, callHandler).subscribe({
-          next: () => {
-            throw new Error('Logging data in error');
-          },
-          error: () => {
-            expect(delegateSpy).toBeCalledWith(new Error('Big oof'), ctxMock, 0, {
-              json: false,
-              color: false,
-              http: true,
-            });
-            expect(logMock).toBeCalledTimes(1);
-            done();
-          },
-        });
-      });
-      it('should not log error', (done) => {
-        jest.spyOn(reflector, 'get').mockReturnValueOnce(true);
-        interceptor.intercept(createMock<ExecutionContext>(), callHandler).subscribe({
-          next: () => {
-            throw new Error('Logging data in error');
-          },
-          error: () => {
-            expect(delegateSpy).toBeCalledTimes(0);
-            expect(logMock).toBeCalledTimes(0);
-            done();
-          },
-        });
-      });
-      it('should log the error and the meta', (done) => {
-        delegateSpy = jest.spyOn(delegator, 'getContextErrorString').mockReturnValueOnce({
-          log: 'error',
-          meta: { key: 'value' },
-        });
-        const ctxMock = createMock<ExecutionContext>(httpContext as Partial<ExecutionContext>);
-        interceptor.intercept(ctxMock, callHandler).subscribe({
-          next: () => {
-            throw new Error('Logging data in error');
-          },
-          error: () => {
-            expect(delegateSpy).toBeCalledWith(new Error('Big oof'), ctxMock, 0, {
-              json: false,
-              color: false,
-              http: true,
-            });
-            expect(logMock).toBeCalledTimes(2);
-            done();
-          },
-        });
-      });
-    });
-  });
-  describe('should skip', () => {
-    it('should skip for class decoration', () => {
-      @OgmaSkip()
-      class TestClass {}
-      jest.spyOn(reflector, 'get').mockReturnValueOnce(true);
-      const ctxMock = createMock<ExecutionContext>({
-        getClass: () => TestClass,
-      });
-      expect(interceptor.shouldSkip(ctxMock)).toBe(true);
-    });
-    it('should skip for method decoration', () => {
-      class TestClass {
-        @OgmaSkip()
-        shouldSkip() {
-          console.log('skip this');
-        }
-      }
-      const test = new TestClass();
-      jest.spyOn(reflector, 'get').mockReturnValueOnce(true);
-      const ctxMock = createMock<ExecutionContext>({
-        getHandler: () => test.shouldSkip.name,
-      });
-      expect(interceptor.shouldSkip(ctxMock)).toBe(true);
-    });
-    it(shouldSkipFor('http'), () => {
-      nullifyOption('http', interceptor);
-      const ctxMock = createMock<ExecutionContext>({
-        getType: () => 'http',
-      });
-      expect(interceptor.shouldSkip(ctxMock)).toBe(true);
-    });
-    it(shouldSkipFor('gql'), () => {
-      nullifyOption('gql', interceptor);
-      const ctxMock = createMock<ExecutionContext>({
-        getType: () => 'graphql',
-      });
-      expect(interceptor.shouldSkip(ctxMock)).toBe(true);
-    });
-    it(shouldSkipFor('ws'), () => {
-      nullifyOption('ws', interceptor);
-      const ctxMock = createMock<ExecutionContext>({
-        getType: () => 'ws',
-      });
-      expect(interceptor.shouldSkip(ctxMock)).toBe(true);
-    });
-    it(shouldSkipFor('rpc'), () => {
-      nullifyOption('rpc', interceptor);
-      const ctxMock = createMock<ExecutionContext>({
-        getType: () => 'rpc',
-      });
-      expect(interceptor.shouldSkip(ctxMock)).toBe(true);
-    });
-  });
-  describe('log', () => {
-    it('should log a value', () => {
-      const logSpy = jest.spyOn(ogmaService, 'info');
-      const ctxMock = createMock<ExecutionContext>({
-        getClass: () => ({
-          name: 'className',
-        }),
-        getHandler: () => ({
-          name: 'methodName',
-        }),
-      });
-      interceptor.log('logValue', ctxMock, '1598961763272766');
-      expect(logSpy).toBeCalledWith('logValue', {
-        context: 'className#methodName',
-        correlationId: '1598961763272766',
-      });
+        ]);
+        is(logMock.callCount, 1);
+        resolve();
+      },
     });
   });
 });
+OgmaInterceptorSuite(
+  'should not log the error',
+  async ({ interceptor, logMock, delegator, reflectorGetSpy }) => {
+    return new Promise<void>((resolve, reject) => {
+      reflectorGetSpy.returns(true);
+      const ctxMock = createCtxMock(httpContext);
+      const delegateSpy = stubMethod(delegator, 'getContextErrorString');
+      delegateSpy.returns({
+        log: 'error',
+      });
+      interceptor.intercept(ctxMock, errorCallHandler).subscribe({
+        next: reject,
+        error: () => {
+          is(delegateSpy.callCount, 0);
+          is(logMock.callCount, 0);
+          resolve();
+        },
+      });
+    });
+  },
+);
+OgmaInterceptorSuite(
+  'should log the error and the meta',
+  async ({ interceptor, logMock, delegator }) => {
+    return new Promise<void>((resolve, reject) => {
+      const ctxMock = createCtxMock(httpContext);
+      const delegateSpy = stubMethod(delegator, 'getContextErrorString');
+      delegateSpy.returns({
+        log: 'error',
+        meta: { key: 'value' },
+      });
+      interceptor.intercept(ctxMock, errorCallHandler).subscribe({
+        next: reject,
+        error: () => {
+          is(delegateSpy.callCount, 1);
+          equal(delegateSpy.firstCall.args, [
+            new Error('Big oof'),
+            ctxMock,
+            0,
+            {
+              json: false,
+              color: false,
+              http: true,
+            },
+          ]);
+          is(logMock.callCount, 2);
+          resolve();
+        },
+      });
+    });
+  },
+);
+OgmaInterceptorSuite('should skip for class decorator', ({ reflectorGetSpy, interceptor }) => {
+  @OgmaSkip()
+  class TestClass {}
+  reflectorGetSpy.returns(true);
+  const ctxMock = createCtxMock({
+    getClass: () => TestClass as any,
+  });
+  ok(interceptor.shouldSkip(ctxMock));
+});
+OgmaInterceptorSuite('should skip for method decorator', ({ reflectorGetSpy, interceptor }) => {
+  class TestClass {
+    @OgmaSkip()
+    shouldSkip() {
+      console.log('skip this');
+    }
+  }
+  const test = new TestClass();
+  reflectorGetSpy.returns(true);
+  const ctxMock = createCtxMock({
+    getHandler: () => test.shouldSkip.name as any,
+  });
+  ok(interceptor.shouldSkip(ctxMock));
+});
+for (const type of ['http', 'gql', 'ws', 'rpc'] as const) {
+  OgmaInterceptorSuite(`Should skip for ${type}`, ({ interceptor }) => {
+    const interceptorOptions = (interceptor as any).options;
+    nullifyOption(type, interceptor);
+    const ctxMock = createCtxMock({
+      getType: () => (type === 'gql' ? 'graphql' : type) as any,
+    });
+    ok(interceptor.shouldSkip(ctxMock));
+    (interceptor as any).options = interceptorOptions;
+  });
+}
+OgmaInterceptorSuite('log should log a val', ({ logMock, interceptor }) => {
+  const ctxMock = createCtxMock({
+    getClass: () => ({ name: 'className' }),
+    getHandler: () => ({ name: 'methodName' }),
+  } as any);
+  interceptor.log('logValue', ctxMock, '1598961763272766');
+  equal(logMock.firstCall.args, [
+    'logValue',
+    { context: 'className#methodName', correlationId: '1598961763272766' },
+  ]);
+});
+OgmaInterceptorSuite.run();
