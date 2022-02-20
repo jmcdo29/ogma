23e20852610dd5440dc01500deb7c3983287255d
Jay McDoniel <jmcdo29@gmail.com>
2022-02-19 17:16:52 -0800

    test(gql): replace platform graphql service test with uvu

--- packages/platform-graphql/test/graphql-interceptor.service.spec.ts
+++ packages/platform-graphql/test/graphql-interceptor.service.spec.ts
@@ -1,22 +1,59 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { Request, Response } from 'express';
+import { spy } from 'hanbi';
+import { suite } from 'uvu';
+import { equal, is } from 'uvu/assert';
 import { GraphQLParser } from '../src';
 
-const gqlMockFactory = (context: Record<string, any>, info: Record<string, any>) =>
-  createMock<ExecutionContext>({
+const gqlMockFactory = (
+  context: Record<string, any>,
+  info: Record<string, any>,
+): ExecutionContext =>
+  ({
     getType: () => 'graphql',
     getHandler: () => 'query',
     getClass: () => 'Test',
     getArgs: () => [{}, {}, context, info],
-  });
+    getArgByIndex: spy().handler,
+    switchToHttp: () => spy().handler,
+    switchToRpc: () => spy().handler,
+    switchToWs: () => spy().handler,
+  } as any);
 
 const gqlContextMockFactory = (contextMock: any) => gqlMockFactory(contextMock, {});
 
 const gqlInfoMockFactory = (infoMock: any) => gqlMockFactory({}, infoMock);
 
-describe('GraphQLParser', () => {
+const GqlParserSuite = suite<{ parser: GraphQLParser }>('GraphQL Parser Suite', {
+  parser: undefined,
+});
+GqlParserSuite.before(async (context) => {
+  const mod = await Test.createTestingModule({
+    providers: [GraphQLParser],
+  }).compile();
+  context.parser = mod.get(GraphQLParser);
+});
+for (const op in ['query', 'mutation']) {
+  GqlParserSuite(`getMethod ${op}`, ({ parser }) => {
+    const gqlContext = gqlInfoMockFactory({
+      operation: { operation: op },
+    });
+    is(parser.getMethod(gqlContext), op);
+  });
+}
+GqlParserSuite('getRequest', ({ parser }) => {
+  const reqMock = { key: 'value' };
+  const gqlCtx = gqlContextMockFactory({ req: reqMock });
+  equal(parser.getRequest(gqlCtx), reqMock);
+});
+GqlParserSuite('getResponse', ({ parser }) => {
+  const resMock = { key: 'value' };
+  const gqlCtx = gqlContextMockFactory({ res: resMock });
+  equal(parser.getResponse(gqlCtx), resMock);
+});
+GqlParserSuite.run();
+
+/* describe('GraphQLParser', () => {
   let parser: GraphQLParser;
 
   beforeEach(async () => {
@@ -54,4 +91,4 @@ describe('GraphQLParser', () => {
       expect(parser.getResponse(gqlCtx)).toEqual(resMock);
     });
   });
-});
+}); */
