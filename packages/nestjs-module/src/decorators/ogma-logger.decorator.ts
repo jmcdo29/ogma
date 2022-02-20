dc79b05de056e3a32056e3385fb09eb42ec60a9d
Jay McDoniel <jmcdo29@gmail.com>
2022-02-20 12:09:14 -0800

    feat(module): create a decorator for auto logging
    There is a new `@Log()` decorator that can now be used to auto
    log the start and stop of a function. Currently it is set to use
    
    `log` but I'll make it configurable to use `debug` or something
    else, possibly calling an `OgmaService` method to make it
    configurable via the `OgmaModule.forRoot()` which would be really
    powerful. I also want to add a `@LogAll()` that applies the
    `@Log()` decorator to all of the methods of the class. These
    are both additions to copme after finishing the `uvu` migration.

--- packages/nestjs-module/src/decorators/ogma-logger.decorator.ts
+++ packages/nestjs-module/src/decorators/ogma-logger.decorator.ts
@@ -1,4 +1,5 @@
 import { Inject } from '@nestjs/common';
+import { isObservable, tap } from 'rxjs';
 
 import { Type } from '../interfaces';
 import { createProviderToken, createRequestScopedProviderToken } from '../ogma.provider';
@@ -8,3 +9,40 @@ export const OgmaLogger = (topic: string | (() => any) | Type<any>) =>
 
 export const OgmaLoggerRequestScoped = (topic: string | (() => any) | Type<any>) =>
   Inject(createRequestScopedProviderToken(typeof topic === 'function' ? topic.name : topic));
+
+const logStart = (context: string, logger: { log: (...args: unknown[]) => void }) => {
+  const start = Date.now();
+  logger.log(`Start ${context}`);
+  return start;
+};
+const logEnd = (
+  context: string,
+  logger: { log: (...args: unknown[]) => void },
+  startTime: number,
+) => {
+  const timing = Date.now() - startTime;
+  logger.log(`End ${context} - ${timing}ms`);
+};
+
+export const Log =
+  (loggerProperty = 'logger'): MethodDecorator =>
+  (target, propertyKey, descriptor) => {
+    const impl = descriptor.value;
+    descriptor.value = function (...args: unknown[]) {
+      const logger = this[loggerProperty];
+      const context = `${target.constructor.name}#${propertyKey.toString()}`;
+      const start = logStart(context, logger);
+      const result = (impl as any).apply(this, ...args);
+      if (result.then) {
+        result.finally(() => {
+          logEnd(context, logger, start);
+        });
+      } else if (isObservable(result)) {
+        result.pipe(tap(() => logEnd(context, logger, start)));
+      } else {
+        logEnd(context, logger, start);
+      }
+      return result;
+    } as any;
+    return descriptor;
+  };
