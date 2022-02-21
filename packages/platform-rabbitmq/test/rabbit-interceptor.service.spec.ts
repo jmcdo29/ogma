88d612fada2c09a0d233bb6c5c8468beb256e730
Jay McDoniel <jmcdo29@gmail.com>
2022-02-21 09:37:17 -0800

    test(rmq): change test runner to uvu

--- packages/platform-rabbitmq/test/rabbit-interceptor.service.spec.ts
+++ packages/platform-rabbitmq/test/rabbit-interceptor.service.spec.ts
@@ -1,69 +1,89 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
 import { style } from '@ogma/styler';
+import { spy } from 'hanbi';
+import { suite } from 'uvu';
+import { is } from 'uvu/assert';
 import { RabbitMqParser } from '../src';
 
-describe('RabbitMqParser', () => {
-  let parser: RabbitMqParser;
-
-  beforeEach(async () => {
-    const modRef = await Test.createTestingModule({
-      providers: [RabbitMqParser],
-    }).compile();
-    parser = modRef.get(RabbitMqParser);
-  });
-  describe('getCallPoint', () => {
-    it('should get the subject from the client', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getPattern: () => JSON.stringify({ cmd: 'message' }),
-          }),
-        }),
-      });
-      expect(parser.getCallPoint(ctxMock)).toBe(JSON.stringify({ cmd: 'message' }));
-    });
-  });
-  describe('getCallerIp', () => {
-    it('should return an ip from data', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({
-            ip: '127.0.0.1',
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('127.0.0.1');
-    });
-    it('should return a blank string', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({}),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('');
-    });
-  });
-  describe('getMethod', () => {
-    it('should return "RabbitMQ"', () => {
-      expect(parser.getMethod()).toBe('RabbitMQ');
-    });
-  });
-  describe('getStatus', () => {
-    it('should return a 200', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false)).toBe('200');
-    });
-    it('should return a 500', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false, new Error())).toBe('500');
-    });
-    it('should return a 200 in color', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), true)).toBe(style.green.apply(200));
-    });
-  });
-  describe('getProtocol', () => {
-    it('should return "amqp"', () => {
-      expect(parser.getProtocol()).toBe('amqp');
-    });
-  });
+const createCtxMock = (partial: Partial<ExecutionContext> = {}): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: spy().handler,
+  getHandler: spy().handler,
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
 });
+
+const RabbitMqParserSuite = suite<{ parser: RabbitMqParser }>('RabbitMq Parser Suite', {
+  parser: undefined,
+});
+RabbitMqParserSuite.before(async (context) => {
+  const modRef = await Test.createTestingModule({
+    providers: [
+      RabbitMqParser,
+      {
+        provide: Reflector,
+        useValue: {
+          get: spy().handler,
+        },
+      },
+    ],
+  }).compile();
+  context.parser = modRef.get(RabbitMqParser);
+});
+RabbitMqParserSuite('it should return the reflected metadata pattern', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getContext: () => ({
+          getPattern: () => JSON.stringify({ cmd: 'message' }),
+        }),
+      } as any),
+  });
+  is(parser.getCallPoint(ctxMock), JSON.stringify({ cmd: 'message' }));
+});
+RabbitMqParserSuite('It should get the ip from the data', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getData: () =>
+          ({
+            ip: '127.0.0.1',
+          } as any),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '127.0.0.1');
+});
+RabbitMqParserSuite('It should return an empty string', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getData: () =>
+          ({
+            hello: 'world',
+          } as any),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '');
+});
+RabbitMqParserSuite('It should return "RabbitMQ"', ({ parser }) => {
+  is(parser.getMethod(), 'RabbitMQ');
+});
+RabbitMqParserSuite('It should return a 200', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false), '200');
+});
+RabbitMqParserSuite('It should return a 500', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false, new Error()), '500');
+});
+RabbitMqParserSuite('It should return a 200 in color', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), true), style.green.apply('200'));
+});
+RabbitMqParserSuite('It should return "amqp" for the protocol', ({ parser }) => {
+  is(parser.getProtocol(), 'amqp');
+});
+RabbitMqParserSuite.run();
