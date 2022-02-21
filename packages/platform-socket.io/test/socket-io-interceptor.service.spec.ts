69ddf4c2b24f5692e2c3d982f8c40e225f832e8a
Jay McDoniel <jmcdo29@gmail.com>
2022-02-21 10:39:10 -0800

    test(socket.io): change test runner to uvu

--- packages/platform-socket.io/test/socket-io-interceptor.service.spec.ts
+++ packages/platform-socket.io/test/socket-io-interceptor.service.spec.ts
@@ -1,74 +1,84 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
 import { MESSAGE_METADATA } from '@nestjs/websockets/constants';
 import { style } from '@ogma/styler';
+import { spy, Stub } from 'hanbi';
+import { suite } from 'uvu';
+import { equal, is } from 'uvu/assert';
 import { SocketIOParser } from '../src';
 
-describe('SocketIOParser', () => {
-  let parser: SocketIOParser;
-  let reflector: Reflector;
-
-  beforeEach(async () => {
-    const mod = await Test.createTestingModule({
-      providers: [
-        SocketIOParser,
-        {
-          provide: Reflector,
-          useValue: {
-            get: jest.fn(() => 'message'),
-          },
-        },
-      ],
-    }).compile();
-    parser = mod.get(SocketIOParser);
-    reflector = mod.get(Reflector);
-  });
-
-  describe('getCallPoint', () => {
-    it('should get the reflected message', () => {
-      const funcMock = () => 'string';
-      const ctxMock = createMock<ExecutionContext>({
-        getHandler: () => funcMock(),
-      });
-      expect(parser.getCallPoint(ctxMock)).toBe('message');
-      expect(reflector.get).toBeCalledWith(MESSAGE_METADATA, funcMock());
-    });
-  });
-  describe('getCallerIp', () => {
-    it('should get the ip from handshake', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToWs: () => ({
-          getClient: () => ({
-            handshake: {
-              address: '::1',
-            },
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('::1');
-    });
-  });
-  describe('getProtocol', () => {
-    it('should return ws', () => {
-      expect(parser.getProtocol()).toBe('WS');
-    });
-  });
-  describe('getMethod', () => {
-    it('should return socket.io', () => {
-      expect(parser.getMethod()).toBe('socket.io');
-    });
-  });
-  describe('getStatus', () => {
-    it('should return a 200', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false)).toBe('200');
-    });
-    it('should return a 500', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false, new Error())).toBe('500');
-    });
-    it('should return a 200 in color', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), true)).toBe(style.green.apply(200));
-    });
-  });
+const createCtxMock = (partial: Partial<ExecutionContext> = {}): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: spy().handler,
+  getHandler: spy().handler,
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
 });
+
+const SocketIOParserSuite = suite<{
+  parser: SocketIOParser;
+  reflectorGetSpy: Stub<Reflector['get']>;
+}>('SocketIO Parser Suite', {
+  parser: undefined,
+  reflectorGetSpy: spy(),
+});
+SocketIOParserSuite.before(async (context) => {
+  const modRef = await Test.createTestingModule({
+    providers: [
+      SocketIOParser,
+      {
+        provide: Reflector,
+        useValue: {
+          get: context.reflectorGetSpy.handler,
+        },
+      },
+    ],
+  }).compile();
+  context.parser = modRef.get(SocketIOParser);
+});
+SocketIOParserSuite(
+  'it should return the reflected metadata pattern',
+  ({ parser, reflectorGetSpy }) => {
+    const funcMock = () => 'string';
+    reflectorGetSpy.returns('message');
+    const ctxMock = createCtxMock({
+      getHandler: () => funcMock,
+    });
+    is(parser.getCallPoint(ctxMock), 'message');
+    equal(reflectorGetSpy.firstCall.args, [MESSAGE_METADATA, funcMock]);
+  },
+);
+SocketIOParserSuite('It should get the ip from the data', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToWs: () =>
+      ({
+        getClient: () => ({
+          handshake: {
+            address: '::1',
+          },
+        }),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '::1');
+});
+SocketIOParserSuite('It should return "socket.io"', ({ parser }) => {
+  is(parser.getMethod(), 'socket.io');
+});
+SocketIOParserSuite('It should return a 200', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false), '200');
+});
+SocketIOParserSuite('It should return a 500', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false, new Error()), '500');
+});
+SocketIOParserSuite('It should return a 200 in color', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), true), style.green.apply('200'));
+});
+SocketIOParserSuite('It should return "WS" for the protocol', ({ parser }) => {
+  is(parser.getProtocol(), 'WS');
+});
+SocketIOParserSuite.run();
