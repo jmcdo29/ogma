98d86e110c29e70b7650c070b3ccb01c7307266a
Jay McDoniel <jmcdo29@gmail.com>
2022-02-21 09:54:20 -0800

    test(redis): change test runner to uvu

--- packages/platform-redis/test/redis-interceptor.service.spec.ts
+++ packages/platform-redis/test/redis-interceptor.service.spec.ts
@@ -1,80 +1,91 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { PATTERN_METADATA } from '@nestjs/microservices/constants';
 import { Test } from '@nestjs/testing';
 import { style } from '@ogma/styler';
+import { spy, Stub } from 'hanbi';
+import { suite } from 'uvu';
+import { equal, is } from 'uvu/assert';
 import { RedisParser } from '../src';
 
-describe('RedisParser', () => {
-  let parser: RedisParser;
-  let reflector: Reflector;
-
-  beforeEach(async () => {
-    const mod = await Test.createTestingModule({
-      providers: [
-        RedisParser,
-        {
-          provide: Reflector,
-          useValue: {
-            get: jest.fn(() => 'message'),
-          },
-        },
-      ],
-    }).compile();
-    parser = mod.get(RedisParser);
-    reflector = mod.get(Reflector);
-  });
-
-  describe('getCallPoint', () => {
-    it('should get the reflected message', () => {
-      const funcMock = () => 'string';
-      const ctxMock = createMock<ExecutionContext>({
-        getHandler: () => funcMock(),
-      });
-      expect(parser.getCallPoint(ctxMock)).toBe(JSON.stringify('message'));
-      expect(reflector.get).toBeCalledWith(PATTERN_METADATA, funcMock());
-    });
-  });
-  describe('getCallerIp', () => {
-    it('should return an empty string', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({}),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('');
-    });
-    it('should return the ip', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({
-            ip: '127.0.0.1',
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('127.0.0.1');
-    });
-  });
-  describe('getProtocol', () => {
-    it('should return "redis"', () => {
-      expect(parser.getProtocol()).toBe('redis');
-    });
-  });
-  describe('getMethod', () => {
-    it('should return REDIS', () => {
-      expect(parser.getMethod()).toBe('REDIS');
-    });
-  });
-  describe('getStatus', () => {
-    it('should return a 200', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false)).toBe('200');
-    });
-    it('should return a 500', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false, new Error())).toBe('500');
-    });
-    it('should return a 200 in color', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), true)).toBe(style.green.apply(200));
-    });
-  });
+const createCtxMock = (partial: Partial<ExecutionContext> = {}): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: spy().handler,
+  getHandler: spy().handler,
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
 });
+
+const RedisParserSuite = suite<{ parser: RedisParser; reflectorGetSpy: Stub<Reflector['get']> }>(
+  'Redis Parser Suite',
+  {
+    parser: undefined,
+    reflectorGetSpy: spy(),
+  },
+);
+RedisParserSuite.before(async (context) => {
+  const modRef = await Test.createTestingModule({
+    providers: [
+      RedisParser,
+      {
+        provide: Reflector,
+        useValue: {
+          get: context.reflectorGetSpy.handler,
+        },
+      },
+    ],
+  }).compile();
+  context.parser = modRef.get(RedisParser);
+});
+RedisParserSuite(
+  'it should return the reflected metadata pattern',
+  ({ parser, reflectorGetSpy }) => {
+    const funcMock = () => 'string';
+    reflectorGetSpy.returns('message');
+    const ctxMock = createCtxMock({
+      getHandler: () => funcMock,
+    });
+    is(parser.getCallPoint(ctxMock), JSON.stringify('message'));
+    equal(reflectorGetSpy.firstCall.args, [PATTERN_METADATA, funcMock]);
+  },
+);
+RedisParserSuite('It should get the ip from the data', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getData: () => ({}),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '127.0.0.1');
+});
+RedisParserSuite('It should return an empty string', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getData: () => ({
+          ip: '127.0.0.1',
+        }),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '');
+});
+RedisParserSuite('It should return "REDIS"', ({ parser }) => {
+  is(parser.getMethod(), 'REDIS');
+});
+RedisParserSuite('It should return a 200', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false), '200');
+});
+RedisParserSuite('It should return a 500', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false, new Error()), '500');
+});
+RedisParserSuite('It should return a 200 in color', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), true), style.green.apply('200'));
+});
+RedisParserSuite('It should return "redis" for the protocol', ({ parser }) => {
+  is(parser.getProtocol(), 'redis');
+});
+RedisParserSuite.run();
