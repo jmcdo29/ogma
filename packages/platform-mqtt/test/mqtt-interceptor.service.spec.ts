813566af6309c82418503bb781877b9601a0b976
Jay McDoniel <jmcdo29@gmail.com>
2022-02-20 19:35:52 -0800

    test(mqtt): change test runner to uvu

--- packages/platform-mqtt/test/mqtt-interceptor.service.spec.ts
+++ packages/platform-mqtt/test/mqtt-interceptor.service.spec.ts
@@ -1,97 +1,97 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import { MqttParser } from '../src';
 import { Reflector } from '@nestjs/core';
 import { PATTERN_METADATA } from '@nestjs/microservices/constants';
+import { Test } from '@nestjs/testing';
 import { style } from '@ogma/styler';
+import { spy, Stub } from 'hanbi';
+import { suite } from 'uvu';
+import { equal, is } from 'uvu/assert';
+import { MqttParser } from '../src';
 
-describe('MQTTParser', () => {
-  let parser: MqttParser;
-  let reflector: Reflector;
-
-  beforeEach(async () => {
-    const modRef = await Test.createTestingModule({
-      providers: [
-        MqttParser,
-        {
-          provide: Reflector,
-          useValue: {
-            get: jest.fn(() => 'message'),
-          },
-        },
-      ],
-    }).compile();
-    parser = modRef.get(MqttParser);
-    reflector = modRef.get(Reflector);
-  });
-  describe('getCallPoint', () => {
-    it('should get the reflected message', () => {
-      const funcMock = () => 'string';
-      const ctxMock = createMock<ExecutionContext>({
-        getHandler: funcMock,
-      });
-      expect(parser.getCallPoint(ctxMock)).toBe(JSON.stringify('message'));
-      expect(reflector.get).toBeCalledWith(PATTERN_METADATA, funcMock());
-    });
-  });
-  describe('getCallerIp', () => {
-    it('should return an ip from the payload', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getPacket: () => ({
-              payload: Buffer.from(JSON.stringify({ data: { hello: 'world', ip: '127.0.0.1' } })),
-            }),
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('127.0.0.1');
-    });
-    it('should return an empty string from the payload without ip as a prop', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getPacket: () => ({
-              payload: Buffer.from(JSON.stringify({ data: { hello: 'world' } })),
-            }),
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('');
-    });
-    it('should return an empty string', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getPacket: () => ({
-              payload: Buffer.from(JSON.stringify({})),
-            }),
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('');
-    });
-  });
-  describe('getMethod', () => {
-    it('should return "MQTT"', () => {
-      expect(parser.getMethod()).toBe('MQTT');
-    });
-  });
-  describe('getStatus', () => {
-    it('should return a 200', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false)).toBe('200');
-    });
-    it('should return a 500', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false, new Error())).toBe('500');
-    });
-    it('should return a 200 in color', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), true)).toBe(style.green.apply(200));
-    });
-  });
-  describe('getProtocol', () => {
-    it('should return "mqtt"', () => {
-      expect(parser.getProtocol()).toBe('mqtt');
-    });
-  });
+const createCtxMock = (partial: Partial<ExecutionContext> = {}): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: spy().handler,
+  getHandler: spy().handler,
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
 });
+
+const MqttParserSuite = suite<{ parser: MqttParser; reflectorGetSpy: Stub<Reflector['get']> }>(
+  'Mqtt Parser Suite',
+  {
+    parser: undefined,
+    reflectorGetSpy: spy(),
+  },
+);
+MqttParserSuite.before(async (context) => {
+  const modRef = await Test.createTestingModule({
+    providers: [
+      MqttParser,
+      {
+        provide: Reflector,
+        useValue: {
+          get: context.reflectorGetSpy.handler,
+        },
+      },
+    ],
+  }).compile();
+  context.parser = modRef.get(MqttParser);
+});
+MqttParserSuite(
+  'it should return the reflected metadata pattern',
+  ({ parser, reflectorGetSpy }) => {
+    const funcMock = () => 'string';
+    reflectorGetSpy.returns('message');
+    const ctxMock = createCtxMock({
+      getHandler: () => funcMock,
+    });
+    is(parser.getCallPoint(ctxMock), JSON.stringify('message'));
+    equal(reflectorGetSpy.firstCall.args, [PATTERN_METADATA, funcMock]);
+  },
+);
+MqttParserSuite('It should get the ip from the data', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getContext: () => ({
+          getPacket: () => ({
+            payload: Buffer.from(JSON.stringify({ data: { hello: 'world', ip: '127.0.0.1' } })),
+          }),
+        }),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '127.0.0.1');
+});
+MqttParserSuite('It should return an empty string', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getContext: () => ({
+          getPacket: () => ({
+            payload: Buffer.from(JSON.stringify({ data: { hello: 'world' } })),
+          }),
+        }),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '');
+});
+MqttParserSuite('It should return "MQTT"', ({ parser }) => {
+  is(parser.getMethod(), 'MQTT');
+});
+MqttParserSuite('It should return a 200', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false), '200');
+});
+MqttParserSuite('It should return a 500', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false, new Error()), '500');
+});
+MqttParserSuite('It should return a 200 in color', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), true), style.green.apply('200'));
+});
+MqttParserSuite('It should return "mqtt" for the protocol', ({ parser }) => {
+  is(parser.getProtocol(), 'mqtt');
+});
+MqttParserSuite.run();
