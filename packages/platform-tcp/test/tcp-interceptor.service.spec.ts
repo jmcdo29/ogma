87ea2936eb5516919a05f290b53e063ee336a2f4
Jay McDoniel <jmcdo29@gmail.com>
2022-02-21 10:15:35 -0800

    test(tcp): change test runner to uvu

--- packages/platform-tcp/test/tcp-interceptor.service.spec.ts
+++ packages/platform-tcp/test/tcp-interceptor.service.spec.ts
@@ -1,88 +1,96 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { PATTERN_METADATA } from '@nestjs/microservices/constants';
 import { Test } from '@nestjs/testing';
 import { style } from '@ogma/styler';
+import { spy, Stub } from 'hanbi';
+import { suite } from 'uvu';
+import { equal, is } from 'uvu/assert';
 import { TcpParser } from '../src';
 
-describe('TcpParser', () => {
-  let parser: TcpParser;
-  let reflector: Reflector;
-
-  beforeEach(async () => {
-    const mod = await Test.createTestingModule({
-      providers: [
-        TcpParser,
-        {
-          provide: Reflector,
-          useValue: {
-            get: jest.fn(() => 'message'),
-          },
-        },
-      ],
-    }).compile();
-    parser = mod.get(TcpParser);
-    reflector = mod.get(Reflector);
-  });
-
-  describe('getCallPoint', () => {
-    it('should get the reflected message', () => {
-      const funcMock = () => 'string';
-      const ctxMock = createMock<ExecutionContext>({
-        getHandler: () => funcMock(),
-      });
-      expect(parser.getCallPoint(ctxMock)).toBe(JSON.stringify('message'));
-      expect(reflector.get).toBeCalledWith(PATTERN_METADATA, funcMock());
-    });
-  });
-  describe('getCallerIp', () => {
-    it('should get the ip from handshake', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getSocketRef: () => ({
-              socket: {
-                remoteAddress: '127.0.0.1',
-                remotePort: '3333',
-              },
-            }),
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('127.0.0.1:3333');
-    });
-  });
-  describe('getProtocol', () => {
-    it('should return IPv4', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getSocketRef: () => ({
-              socket: {
-                remoteFamily: 'IPv4',
-              },
-            }),
-          }),
-        }),
-      });
-      expect(parser.getProtocol(ctxMock)).toBe('IPv4');
-    });
-  });
-  describe('getMethod', () => {
-    it('should return TCP', () => {
-      expect(parser.getMethod()).toBe('TCP');
-    });
-  });
-  describe('getStatus', () => {
-    it('should return a 200', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false)).toBe('200');
-    });
-    it('should return a 500', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false, new Error())).toBe('500');
-    });
-    it('should return a 200 in color', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), true)).toBe(style.green.apply(200));
-    });
-  });
+const createCtxMock = (partial: Partial<ExecutionContext> = {}): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: spy().handler,
+  getHandler: spy().handler,
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
 });
+
+const TcpParserSuite = suite<{ parser: TcpParser; reflectorGetSpy: Stub<Reflector['get']> }>(
+  'Tcp Parser Suite',
+  {
+    parser: undefined,
+    reflectorGetSpy: spy(),
+  },
+);
+TcpParserSuite.before(async (context) => {
+  const modRef = await Test.createTestingModule({
+    providers: [
+      TcpParser,
+      {
+        provide: Reflector,
+        useValue: {
+          get: context.reflectorGetSpy.handler,
+        },
+      },
+    ],
+  }).compile();
+  context.parser = modRef.get(TcpParser);
+});
+TcpParserSuite('it should return the reflected metadata pattern', ({ parser, reflectorGetSpy }) => {
+  const funcMock = () => 'string';
+  reflectorGetSpy.returns('message');
+  const ctxMock = createCtxMock({
+    getHandler: () => funcMock,
+  });
+  is(parser.getCallPoint(ctxMock), JSON.stringify('message'));
+  equal(reflectorGetSpy.firstCall.args, [PATTERN_METADATA, funcMock]);
+});
+TcpParserSuite('It should get the ip from the data', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getContext: () => ({
+          getSocketRef: () => ({
+            socket: {
+              remoteAddress: '127.0.0.1',
+              remotePort: '3333',
+            },
+          }),
+        }),
+      } as any),
+  });
+  is(parser.getCallerIp(ctxMock), '127.0.0.1:3333');
+});
+TcpParserSuite('It should return "REDIS"', ({ parser }) => {
+  is(parser.getMethod(), 'REDIS');
+});
+TcpParserSuite('It should return a 200', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false), '200');
+});
+TcpParserSuite('It should return a 500', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false, new Error()), '500');
+});
+TcpParserSuite('It should return a 200 in color', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), true), style.green.apply('200'));
+});
+TcpParserSuite('It should return "redis" for the protocol', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getContext: () => ({
+          getSocketRef: () => ({
+            socket: {
+              remoteFamily: 'IPv4',
+            },
+          }),
+        }),
+      } as any),
+  });
+  is(parser.getProtocol(ctxMock), 'IPv4');
+});
+TcpParserSuite.run();
