8e189e385b260e00bc2c9aa29412d3cf90d00bbb
Jay McDoniel <jmcdo29@gmail.com>
2022-02-20 19:12:25 -0800

    test(kafka): change test runner to uvu

--- packages/platform-kafka/test/kafka-interceptor.service.spec.ts
+++ packages/platform-kafka/test/kafka-interceptor.service.spec.ts
@@ -1,90 +1,87 @@
-import { createMock } from '@golevelup/ts-jest';
 import { ExecutionContext } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import { KafkaParser } from '../src';
 import { Reflector } from '@nestjs/core';
+import { Test } from '@nestjs/testing';
 import { style } from '@ogma/styler';
+import { spy } from 'hanbi';
+import { suite } from 'uvu';
+import { is } from 'uvu/assert';
+import { KafkaParser } from '../src';
 
-describe('KafkaParser', () => {
-  let parser: KafkaParser;
-
-  beforeEach(async () => {
-    const modRef = await Test.createTestingModule({
-      providers: [
-        KafkaParser,
-        {
-          provide: Reflector,
-          useValue: {
-            get: jest.fn(() => 'message'),
-          },
-        },
-      ],
-    }).compile();
-    parser = modRef.get(KafkaParser);
-  });
-  describe('getCallPoint', () => {
-    it('should get the reflected message', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getContext: () => ({
-            getTopic: () => 'say.hello',
-          }),
-        }),
-      });
-      expect(parser.getCallPoint(ctxMock)).toBe('say.hello');
-    });
-  });
-  describe('getCallerIp', () => {
-    it('should return an ip from the payload', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({
-            value: { hello: 'world', ip: '127.0.0.1' },
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('127.0.0.1');
-    });
-    it('should return an empty string from the payload without ip as a prop', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({
-            value: { hello: 'world' },
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('');
-    });
-    it('should return an empty string', () => {
-      const ctxMock = createMock<ExecutionContext>({
-        switchToRpc: () => ({
-          getData: () => ({
-            value: {},
-          }),
-        }),
-      });
-      expect(parser.getCallerIp(ctxMock)).toBe('');
-    });
-  });
-  describe('getMethod', () => {
-    it('should return "Kafka"', () => {
-      expect(parser.getMethod()).toBe('Kafka');
-    });
-  });
-  describe('getStatus', () => {
-    it('should return a 200', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false)).toBe('200');
-    });
-    it('should return a 500', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), false, new Error())).toBe('500');
-    });
-    it('should return a 200 in color', () => {
-      expect(parser.getStatus(createMock<ExecutionContext>(), true)).toBe(style.green.apply(200));
-    });
-  });
-  describe('getProtocol', () => {
-    it('should return "kafka"', () => {
-      expect(parser.getProtocol()).toBe('kafka');
-    });
-  });
+const createCtxMock = (partial: Partial<ExecutionContext> = {}): ExecutionContext => ({
+  getArgByIndex: spy().handler,
+  getArgs: spy().handler,
+  getClass: spy().handler,
+  getHandler: spy().handler,
+  getType: spy().handler,
+  switchToHttp: spy().handler,
+  switchToRpc: spy().handler,
+  switchToWs: spy().handler,
+  ...partial,
 });
+
+const KafkaParserSuite = suite<{ parser: KafkaParser }>('Kafka Parser Suite', {
+  parser: undefined,
+});
+KafkaParserSuite.before(async (context) => {
+  const modRef = await Test.createTestingModule({
+    providers: [
+      KafkaParser,
+      {
+        provide: Reflector,
+        useValue: {
+          get: spy().handler,
+        },
+      },
+    ],
+  }).compile();
+  context.parser = modRef.get(KafkaParser);
+});
+KafkaParserSuite('it should return the reflected metadata pattern', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () =>
+      ({
+        getContext: () =>
+          ({
+            getTopic: () => 'say.hello',
+          } as any),
+      } as any),
+  });
+  is(parser.getCallPoint(ctxMock), 'say.hello');
+});
+KafkaParserSuite('It should get the ip from the data', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () => ({
+      getData: () =>
+        ({
+          ip: '127.0.0.1',
+        } as any),
+      getContext: spy().handler,
+    }),
+  });
+  is(parser.getCallerIp(ctxMock), '127.0.0.1');
+});
+KafkaParserSuite('It should return an empty string', ({ parser }) => {
+  const ctxMock = createCtxMock({
+    switchToRpc: () => ({
+      getData: () => ({} as any),
+      getContext: spy().handler,
+    }),
+  });
+  is(parser.getCallerIp(ctxMock), '');
+});
+KafkaParserSuite('It should return "Kafka"', ({ parser }) => {
+  is(parser.getMethod(), 'Kafka');
+});
+KafkaParserSuite('It should return a 200', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false), '200');
+});
+KafkaParserSuite('It should return a 500', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), false, new Error()), '500');
+});
+KafkaParserSuite('It should return a 200 in color', ({ parser }) => {
+  is(parser.getStatus(createCtxMock(), true), style.green.apply('200'));
+});
+KafkaParserSuite('It should return "kafka" for the protocol', ({ parser }) => {
+  is(parser.getProtocol(), 'kafka');
+});
+KafkaParserSuite.run();
