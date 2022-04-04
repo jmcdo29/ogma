115a96c61d2524adbb37a64745fa075f880d8efb
Ilya Moroz <ilya.moroz.v@gmail.com>
2022-04-04 20:38:50 +0300

    feat(logger): added ability to remove 'pid', 'hostname', 'application' from output

--- packages/common/src/log.interface.ts
+++ packages/common/src/log.interface.ts
@@ -2,12 +2,12 @@ import { OgmaWritableLevel } from './level.enum';
 
 export interface OgmaLog {
   time: string;
-  hostname: string;
+  hostname?: string;
   application?: string;
   context?: string;
   message?: string;
   level: string;
-  pid: string | number;
+  pid?: string | number;
   ool: OgmaWritableLevel;
   [key: string]: any;
 }
