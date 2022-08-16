a54fd4f70694c435b382b5378ffaf235c650cf94
Jay McDoniel <jmcdo29@gmail.com>
2022-08-16 05:17:03 -0700

    chore: update to latest nest for microservice support in tests

--- integration/src/rpc/client/rpc-client.controller.ts
+++ integration/src/rpc/client/rpc-client.controller.ts
@@ -11,15 +11,11 @@ import { ClientProxy } from '@nestjs/microservices';
 import { ExceptionFilter } from './../../shared/exception.filter';
 
 @Controller()
-export class RpcClientController implements OnApplicationBootstrap, OnApplicationShutdown {
+export class RpcClientController implements OnApplicationBootstrap /*, OnApplicationShutdown */ {
   constructor(@Inject('RPC-SERVICE') private readonly micro: ClientProxy) {}
 
   async onApplicationBootstrap() {
     await this.micro.connect();
-  }
-
-  async onApplicationShutdown() {
-    await this.micro.close();
   }
 
   @Get()
