54f2517766dfe04775730164b91f9b067e6ddd7c
Jay McDoniel <jmcdo29@gmail.com>
2023-06-15 09:25:46 -0700

    fix: use new get pattern on ws requests

--- packages/nestjs-module/src/interceptor/providers/websocket-interceptor.service.ts
+++ packages/nestjs-module/src/interceptor/providers/websocket-interceptor.service.ts
@@ -1,11 +1,11 @@
-import { ArgumentsHost, ExecutionContext, Injectable } from '@nestjs/common';
+import { ArgumentsHost, Injectable } from '@nestjs/common';
 
 import { AbstractInterceptorService } from './abstract-interceptor.service';
 
 @Injectable()
 export abstract class WebsocketInterceptorService extends AbstractInterceptorService {
-  getCallPoint(context: ExecutionContext): string {
-    return this.reflector.get<string>('message', context.getHandler());
+  getCallPoint(context: ArgumentsHost): string {
+    return this.getClient(context).getPattern();
   }
 
   /**
