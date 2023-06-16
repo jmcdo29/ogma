674555dc8957951ffba39b54eb4d2b13351c0ccc
Vinícius Lourenço <vinyygamerlol@gmail.com>
2023-06-15 21:11:13 -0300

    chore(logger): removed unused console log

--- packages/logger/test/ogma.spec.ts
+++ packages/logger/test/ogma.spec.ts
@@ -66,7 +66,6 @@ for (const json of [true, false]) {
       ogma.log('Hello');
       const loggedVal = getFirstCallString(writeSpy);
       if (json) {
-        console.log(loggedVal);
         const loggedJSON = JSON.parse(loggedVal);
         match(loggedJSON.time, /\d{10}/);
         match(loggedJSON.pid, /\d{1,5}/);
