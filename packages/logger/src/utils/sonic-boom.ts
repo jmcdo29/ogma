5ef8664c24f46830e5c01202f74a387a7b57f3ff
Jay McDoniel <jmcdo29@gmail.com>
2023-09-24 10:06:15 -0700

    fix: properly use on-exit-lek-free for worker threads

--- packages/logger/src/utils/sonic-boom.ts
+++ packages/logger/src/utils/sonic-boom.ts
@@ -1,5 +1,7 @@
 import { OgmaStream } from '@ogma/common';
+import * as onExit from 'on-exit-leak-free';
 import SonicBoom from 'sonic-boom';
+import { isMainThread } from 'worker_threads';
 
 const noop = (_args?: any) => true; // eslint-disable-line @typescript-eslint/no-empty-function
 
@@ -7,11 +9,17 @@ const noop = (_args?: any) => true; // eslint-disable-line @typescript-eslint/no
  * thanks pinojs
  * ref: https://github.com/pinojs/pino/blob/27d2ab8b58e64547fd24864c2f21ac898f4752c4/lib/tools.js#L219
  */
-function buildSafeSonicBoom(opts: { fd: typeof process['stdout']['fd'] }) {
-  // the ?? 1 is here to handle worker threads like using vitest
-  const stream = new SonicBoom({ fd: opts.fd ?? 1 });
-
+function buildSafeSonicBoom(opts) {
+  const stream = new SonicBoom(opts);
   stream.on('error', filterBrokenPipe);
+  // if we are sync: false, we must flush on exit
+  if (!opts.sync && isMainThread) {
+    onExit.register(stream, autoEnd);
+
+    stream.on('close', function () {
+      onExit.unregister(stream);
+    });
+  }
   return stream;
 
   function filterBrokenPipe(err) {
@@ -29,6 +37,27 @@ function buildSafeSonicBoom(opts: { fd: typeof process['stdout']['fd'] }) {
     }
     stream.removeListener('error', filterBrokenPipe);
     stream.emit('error', err);
+  }
+}
+
+function autoEnd(stream, eventName) {
+  // This check is needed only on some platforms
+  /* istanbul ignore next */
+  if (stream.destroyed) {
+    return;
+  }
+
+  if (eventName === 'beforeExit') {
+    // We still have an event loop, let's use it
+    stream.flush();
+    stream.on('drain', function () {
+      stream.end();
+    });
+  } else {
+    // For some reason istanbul is not detecting this, but it's there
+    /* istanbul ignore next */
+    // We do not have an event loop, so flush synchronously
+    stream.flushSync();
   }
 }
 
