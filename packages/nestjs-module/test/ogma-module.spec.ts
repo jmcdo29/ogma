6073fc19fca2b557a41902f9ca8b937fd069b9bb
Jay McDoniel <jmcdo29@gmail.com>
2025-01-31 11:05:00 -0800

    chore: update nest deps to v11

--- packages/nestjs-module/test/ogma-module.spec.ts
+++ packages/nestjs-module/test/ogma-module.spec.ts
@@ -2,43 +2,13 @@ import { Test } from '@nestjs/testing';
 import { suite } from 'uvu';
 import { ok } from 'uvu/assert';
 
-import { HttpInterceptorService, OgmaModule, OgmaModuleOptions } from '../src';
-
-class NoopInterceptorService extends HttpInterceptorService {
-  noop() {
-    return 'noop';
-  }
-  getCallPoint = this.noop;
-  getCallerIp = this.noop;
-  getMethod = this.noop;
-  getProtocol = this.noop;
-  setRequestId;
-}
-
-const interceptorOptions = {
-  interceptor: {
-    http: NoopInterceptorService,
-  },
-};
+import { OgmaModule, OgmaModuleOptions } from '../src';
 
 const noIntOptions = {};
 
-const allOptions: OgmaModuleOptions = {
-  service: {
-    color: true,
-    application: 'JEST',
-    context: 'TESTING',
-    json: false,
-  },
-  interceptor: {
-    http: NoopInterceptorService,
-    ws: NoopInterceptorService,
-    gql: NoopInterceptorService,
-    rpc: NoopInterceptorService,
-  },
-};
+const allOptions: OgmaModuleOptions = {};
 const OgmaModuleSuite = suite('Ogma Module');
-for (const options of [interceptorOptions, noIntOptions, undefined, allOptions]) {
+for (const options of [noIntOptions, undefined, allOptions]) {
   OgmaModuleSuite(`It should make the module for ${options}`, async () => {
     const mod = await Test.createTestingModule({
       imports: [OgmaModule.forRoot(options)],
